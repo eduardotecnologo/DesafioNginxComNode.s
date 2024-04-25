3e2a4123b010958f99532c051c34f6781a059eb8
eduardotecnologo <eduardotecnologo@hotmail.com>
2024-04-24 21:00:28 -0300

    fix: api, password

--- db/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS fullcycle;
-USE fullcycle;
-
-CREATE TABLE IF NOT EXISTS people (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
-);
-
-INSERT INTO people (name) VALUES ('Eduardo'), ('Alexandre'), ('Lucio');
